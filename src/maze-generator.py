f7d86ebe114da930ab04d296fcaf1e36eeb4c15b
Bernat Frangi <bernat.frangi@gmail.com>
2022-12-29 00:35:59 +0000

    Update example

--- src/maze-generator.py
+++ src/maze-generator.py
@@ -2,9 +2,9 @@ from lib import MazeColors, MazeGenerator
 import sys
 sys.setrecursionlimit(1000000)
 
-MAZE_MAP_DIMENSIONS = (20, 20)
-INITIAL_POSITION = (19, 0)
-FINAL_POSITION = (0, 19)
+MAZE_MAP_DIMENSIONS = (34, 15)
+INITIAL_POSITION = (0, 0)
+FINAL_POSITION = (33, 14)
 
 
 def main():
@@ -26,31 +26,6 @@ def main():
     # the solution
     maze_str_sol = mg.maze_to_string(show_solution=True)
     print(maze_str_sol)
-
-    # Generate the maze, pausing, printing the maze and logging 
-    # to `log.txt` at every step
-    maze = mg.generate_maze(
-        print_steps=True,
-        log='log.txt',
-        pause=True
-    )
-
-    # Convert the current maze to a printable string
-    # maze_str = mg.maze_to_string(
-    #     space='██',
-    #     color_walls=MazeColors.OKBLUE,
-    #     show_solution=True
-    # )
-    # print(maze_str)
-
-    # Convert the current maze to a printable string and highlight
-    # the solution
-    # maze_str_solved = mg.maze_to_string(
-    #     space='██',
-    #     color_walls=MazeColors.OKBLUE,
-    # )
-    # print(maze_str_solved)
-
 
 if __name__ == '__main__':
     main()
